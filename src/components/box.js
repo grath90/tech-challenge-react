56093747e6ff986fcb43423a53655e62890856bf
Cole Rath <grath90@gmail.com>
2021-11-03 21:03:35 -0500

    added images. added home view and box componenet with supportings css.

---
+++ src/components/box.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './box.css'
+
+const Box = (props) => {
+  return (
+    <div className="box">
+      <img src={`${process.env.PUBLIC_URL}/${props.image}`} class="center" alt=""/>
+      <h3>Heading Two</h3>
+      <p>
+        Lorem ipsum dolar sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor incididunt
+        ut labore et dolore
+      </p>
+      <button>
+        Learn More
+      </button>
+    </div>
+  )
+}
+
+export default Box
---
+++ src/views/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './home.css'
+import Box from '../components/box'
+
+const Home = () => {
+  return (
+    <div className="container">
+      <Box image="Talkie.png"/>
+    </div>
+  )
+}
+
+export default Home
